42a129f41eb4c01c4be358ec6726b4c632382fdb
frainfreeze <kucar.tomislav@gmail.com>
2018-06-13 10:44:58 +0200

    Add files via upload

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo -e " [+] fetching updates"
+apt-get update
+
+echo -e " [+] setting up tools"
+cd /home/src
+/bin/bash setup.sh
+
+echo -e " [+] building the kernel"
+
+
+
+echo -e " [+] box up and running"
+echo -e " [+] you can log in using vagrant ssh"
