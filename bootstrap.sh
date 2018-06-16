ad01b149ab494273571d17e259a16ce298086b1c
frainfreeze <kucar.tomislav@gmail.com>
2018-06-16 15:21:24 +0200

    build tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
-echo -e " [+] fetching updates"
+touch status.log
+function log() {
+    if [ "$QUIET" = "1" ]; then
+        "$@" >> status.log 2>&1
+    else
+        "$@" 2>&1 | tee -a status.log
+    fi
+}
+
+log date
+log echo -e " [+] fetching updates"
 apt-get update
 
-echo -e " [+] building i686 elf tools"
+log echo -e " [+] building i686 elf tools"
 cd /home/src
 /bin/bash i686-elf-tools.sh
 
 #cmake .
 
-echo -e " [+] box up and running"
-echo -e " [+] you can log in using vagrant ssh"
+log echo -e " [+] box up and running"
+log echo -e " [+] you can log in using vagrant ssh"
+log date
